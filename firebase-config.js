c858b608f07a6d6c427d8a00395ee417d34f0be9
siolnei <siolnei@gmail.com>
2025-08-05 12:10:24 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -16,3 +16,17 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 export { db };
+
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    projectId: "SEU_PROJETO",
+    storageBucket: "SEU_PROJETO.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID"
+};
+
+// Inicialização do Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
