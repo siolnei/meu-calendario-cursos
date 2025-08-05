d3bafbf68620f67655a1ffaee7d9cfe71b0d727a
siolnei <siolnei@gmail.com>
2025-08-05 11:18:08 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyD3kjJk0qIvYcxat9QgRk-5Z-tv9S1dSm4",
+  authDomain: "calenario-de-cursos.firebaseapp.com",
+  projectId: "calenario-de-cursos",
+  storageBucket: "calenario-de-cursos.firebasestorage.app",
+  messagingSenderId: "968930225234",
+  appId: "1:968930225234:web:f69ee44bf4ba1bf4ccc38b",
+  measurementId: "G-M51RRXD1QL"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Exporta as referências do Firestore para uso em outros arquivos
+const db = firebase.firestore();
+
+export { db };
